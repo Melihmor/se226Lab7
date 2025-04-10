befbfada16fc3d1eb6342a83b7ef86038551fbe9
Melihmor <152394827+Melihmor@users.noreply.github.com>
2025-04-10 03:31:52 -0700

    math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,22 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return abs(x-y)
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        print("Division by Zero Error")
+
+    else:
+        return x/y
+
+def power(x,y):
+       return x**y
+
+def mod(x,y):
+    return x%y
+
